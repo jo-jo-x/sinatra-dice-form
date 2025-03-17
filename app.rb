0b7241b823b8070764c61e6373200deefb7cc16e
Maurice Ford <mauriceford@gmail.com>
2025-03-17 23:10:43 +0000

    added proccessor code

--- app.rb
+++ app.rb
@@ -7,5 +7,18 @@ end
 
 get("/process_roll") do
   @dicerolls = params
+
+  @num_dice = @dicerolls.fetch("dice").to_i
+
+  @sides = @dicerolls.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
   erb(:processor)
 end
